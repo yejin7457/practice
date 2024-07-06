3a1378b69ff38dcdc20573c4303986016bebeff1
yejin7457 <161455449+yejin7457@users.noreply.github.com>
2024-07-06 18:15:03 +0900

    android studio prac2

---
+++ app/src/main/java/com/example/prac2/DrawingMaskActivity.kt
@@ -0,0 +1,20 @@
+package com.example.prac2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DrawingMaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_drawing_mask)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawing_mask)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
